c5b93e4dcfaee6cbbb2415ad365bc50d757beb96
julianoeibel <juliano.eibel@live.com>
2022-03-25 07:17:44 -0300

    implementação do iframe

--- js/script.js
+++ js/script.js
@@ -10,7 +10,38 @@ $('.owl-carousel').owlCarousel({
             items:5
         },
         1441:{
-            items:10
+            items:8
         }
     }
 })
+
+$(".item-series").click(function() {
+    if ($(window).width() < 426) {
+        $('.iframe').eq($(".item-series").index($(this))-6).toggleClass('open');
+        $('.overlay').addClass('open');
+     } else if ($(window).width() < 769) {
+        $('.iframe').eq($(".item-series").index($(this))-6).toggleClass('open');
+        $('.overlay').addClass('open');
+     } else if ($(window).width() < 1025) {
+        $('.iframe').eq($(".item-series").index($(this))-6).toggleClass('open');
+        $('.overlay').addClass('open');
+     } else if ($(window).width() < 1441) {
+        $('.iframe').eq($(".item-series").index($(this))-6).toggleClass('open');
+        $('.overlay').addClass('open');
+     } else {
+        $('.iframe').eq($(".item-series").index($(this))-8).toggleClass('open');
+        $('.overlay').addClass('open');
+     }
+});
+
+$('.iframe_close').on('click', function() {
+    $(this).closest('.iframe').removeClass('open');
+    $('.overlay').removeClass('open');
+});
+
+$('.overlay').on('click', function() {
+    $('.iframe').removeClass('open');
+    $('.overlay').removeClass('open');
+    $('.menu').removeClass('open');
+});
+
